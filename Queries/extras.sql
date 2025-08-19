65210097fa960df25a3470de8676491dd7c4491e
Mathias Toala <fetoala@outlook.com>
2025-08-18 23:58:09 -0500

    Preparing for new queries

--- Queries/extras.sql
+++ Queries/extras.sql
@@ -1,4 +1,12 @@
 -- Añadir otro campo a la tabla RENTAL llamado "alquilado"
 
 
--- Modificar el sp para obtener las peliculas
+-- Modificar el sp para obtener las 
+
+
+
+-- Usar más sps...? PUEDE SER -> PARA OBNETER INFO DE ACTOR, STAFF Y TIENDA.
+-- No necesario para insert, delete o update
+-- AÑADIR UN TRIGGER PARA BORRAR EL ALQUILER ANTERIOR
+
+-- Boton para cambiar entre catalogo de pelis y todas las disponibles para alquilar
